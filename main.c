84e24ebf41e73ef1abd4927770e6c6b7dd7c74ea
Octogonapus <me@irrational.engineering>
2025-05-06 11:08:42 -0400

    refactor

--- main.c
+++ main.c
@@ -15,20 +15,16 @@
 Based on https://github.com/ajaiantilal/i7z
 GNU General Public License v2.0
 */
-uint64_t get_msr_value(int32_t cpu, uint32_t reg, uint8_t highbit, uint8_t lowbit, int *error_indx) {
-    uint64_t data;
-    int fd;
+uint64_t get_msr_value(int32_t cpu, uint32_t reg, uint8_t highbit, uint8_t lowbit) {
     char msr_file_name[64];
-    int bits;
-    *error_indx = 0;
-
     sprintf(msr_file_name, "/dev/cpu/%d/msr", cpu);
-    fd = open(msr_file_name, O_RDONLY);
+    const int fd = open(msr_file_name, O_RDONLY);
     if (fd < 0) {
-        fprintf(stderr, "Invalid CPU number: %d\n", cpu);
+        fprintf(stderr, "Failed to open CPU: %d\n", cpu);
         exit(1);
     }
 
+    uint64_t data;
     if (pread(fd, &data, sizeof data, reg) != sizeof data) {
         perror("rdmsr:pread");
         exit(127);
@@ -36,7 +32,7 @@ uint64_t get_msr_value(int32_t cpu, uint32_t reg, uint8_t highbit, uint8_t lowbi
 
     close(fd);
 
-    bits = highbit - lowbit + 1;
+    const int bits = highbit - lowbit + 1;
     if (bits < 64) {
         /* Show only part of register */
         data >>= lowbit;
@@ -49,13 +45,11 @@ uint64_t get_msr_value(int32_t cpu, uint32_t reg, uint8_t highbit, uint8_t lowbi
         data = -data;
     }
 
-    *error_indx = 0;
     return (data);
 }
 
 float Read_Voltage_CPU(int32_t cpu_num) {
-    int error_indx;
-    unsigned long val = get_msr_value(cpu_num, MSR_PERF_STATUS, 47, 32, &error_indx);
+    const uint64_t val = get_msr_value(cpu_num, MSR_PERF_STATUS, 47, 32);
     return (float)val / (float)(1 << 13);
 }
 
@@ -67,7 +61,7 @@ int main(int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
         char *endptr;
         errno = 0;
-        long cpu = strtol(argv[i], &endptr, 10);
+        const long cpu = strtol(argv[i], &endptr, 10);
         if (errno != 0 || *endptr != '\0' || cpu < 0 || cpu > INT32_MAX) {
             fprintf(stderr, "Invalid CPU number: %s\n", argv[i]);
             return 1;
