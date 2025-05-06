4238161cff46ac98ededc8b5fd292f3ba8c8154f
Octogonapus <me@irrational.engineering>
2025-05-06 11:05:03 -0400

    Always reject bad CPU files. Fix narrowing bug.

--- main.c
+++ main.c
@@ -12,15 +12,12 @@
 #define MSR_PERF_STATUS 0x198
 
 /*
-Taken from https://github.com/ajaiantilal/i7z
-
+Based on https://github.com/ajaiantilal/i7z
 GNU General Public License v2.0
 */
 uint64_t get_msr_value(int cpu, uint32_t reg, unsigned int highbit, unsigned int lowbit, int *error_indx) {
     uint64_t data;
     int fd;
-    //  char *pat;
-    //  int width;
     char msr_file_name[64];
     int bits;
     *error_indx = 0;
@@ -28,20 +25,8 @@ uint64_t get_msr_value(int cpu, uint32_t reg, unsigned int highbit, unsigned int
     sprintf(msr_file_name, "/dev/cpu/%d/msr", cpu);
     fd = open(msr_file_name, O_RDONLY);
     if (fd < 0) {
-        if (errno == ENXIO) {
-            // fprintf (stderr, "rdmsr: No CPU %d\n", cpu);
-            *error_indx = 1;
-            return 1;
-        } else if (errno == EIO) {
-            // fprintf (stderr, "rdmsr: CPU %d doesn't support MSRs\n", cpu);
-            *error_indx = 1;
-            return 1;
-        } else {
-            // perror ("rdmsr:open");
-            *error_indx = 1;
-            return 1;
-            // exit (127);
-        }
+        fprintf(stderr, "Invalid CPU number: %d\n", cpu);
+        exit(1);
     }
 
     if (pread(fd, &data, sizeof data, reg) != sizeof data) {
@@ -68,7 +53,7 @@ uint64_t get_msr_value(int cpu, uint32_t reg, unsigned int highbit, unsigned int
     return (data);
 }
 
-float Read_Voltage_CPU(int cpu_num) {
+float Read_Voltage_CPU(int32_t cpu_num) {
     int error_indx;
     unsigned long val = get_msr_value(cpu_num, MSR_PERF_STATUS, 47, 32, &error_indx);
     return (float)val / (float)(1 << 13);
@@ -83,13 +68,12 @@ int main(int argc, char **argv) {
         char *endptr;
         errno = 0;
         long cpu = strtol(argv[i], &endptr, 10);
-
-        if (errno != 0 || *endptr != '\0' || cpu < 0) {
+        if (errno != 0 || *endptr != '\0' || cpu < 0 || cpu > INT32_MAX) {
             fprintf(stderr, "Invalid CPU number: %s\n", argv[i]);
             return 1;
         }
 
-        printf("%1.2f\n", Read_Voltage_CPU(cpu));
+        printf("%1.2f\n", Read_Voltage_CPU((int32_t)cpu));
     }
 
     return 0;
